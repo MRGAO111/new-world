aa39625972f6dc4e5457306e706be621ab2763ae
MRGAO111 <32873760+MRGAO111@users.noreply.github.com>
2017-10-18 19:55:32 +0800

    创建成绩评定C文件
    第一次使用git创建文档

---
+++ pingding.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    char f;
+    scanf("%d",&x);
+    if (x>=90)
+        f='A';
+    if ((x>=80)&&(x<90))
+        f='B';
+    if (x<80)
+        f='C';
+    printf("f=%c\n",f);
+    return 0;
+}
+#include<stdio.h>
+int main()
+{
+    int x;
+    char f;
+    scanf("%d",&x);
+    if (x>=90)
+        f='A';
+    if ((x>=80)&&(x<90))
+        f='B';
+    if (x<80)
+        f='C';
+    printf("f=%c\n",f);
+    return 0;
+}
